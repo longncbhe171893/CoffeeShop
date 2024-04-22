970fccff2487329d8a4667b2ee3b7db69305de8f
lovemyself-s <vuhdhe160029@fpt.edu.vn>
2024-04-22 10:51:23 +0700

    .

--- src/java/Controller/Register.java
+++ src/java/Controller/Register.java
@@ -5,14 +5,12 @@
 package Controller;
 
 import DAO.UserDAO;
-import Model.Role;
 import Model.User;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  *
