69eda369444e6f15ddac519123c2087963c6f6c2
longncbhe171893 <longncbhe171893@fpt.edu.vn>
2024-04-27 01:26:47 +0700

    feedback model +dao

---
+++ src/java/Model/Feedback.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author Admin
+ */
+public class Feedback {
+     private int user_id;
+     private String content;
+     private int product_id;
+     private Date post_Date;
+
+    public Feedback() {
+    }
+
+    public Feedback(int user_id, String content, int product_id, Date post_Date) {
+        this.user_id = user_id;
+        this.content = content;
+        this.product_id = product_id;
+        this.post_Date = post_Date;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public Date getPost_Date() {
+        return post_Date;
+    }
+
+    public void setPost_Date(Date post_Date) {
+        this.post_Date = post_Date;
+    }
+
+    @Override
+    public String toString() {
+        return "Feedback{" + "user_id=" + user_id + ", content=" + content + ", product_id=" + product_id + ", post_Date=" + post_Date + '}';
+    }
+     
+}
